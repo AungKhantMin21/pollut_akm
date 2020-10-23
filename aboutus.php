7c0527de297ecd0baf9631b8e2431e7816cc333e
AungKhantMin21 <aungkhantmin212002@gmail.com>
2020-10-23 13:08:49 +0630

    add aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -1,20 +1,93 @@
 <?php 
-    include("header.php");
+    include("layout/header.php");
 ?>
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum quos, ipsa corporis tempore consectetur similique fuga, adipisci a pariatur temporibus voluptates inventore dolores cupiditate aliquid officia quibusdam maxime quia? Accusantium?</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum quos, ipsa corporis tempore consectetur similique fuga, adipisci a pariatur temporibus voluptates inventore dolores cupiditate aliquid officia quibusdam maxime quia? Accusantium?</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum quos, ipsa corporis tempore consectetur similique fuga, adipisci a pariatur temporibus voluptates inventore dolores cupiditate aliquid officia quibusdam maxime quia? Accusantium?</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum quos, ipsa corporis tempore consectetur similique fuga, adipisci a pariatur temporibus voluptates inventore dolores cupiditate aliquid officia quibusdam maxime quia? Accusantium?</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum quos, ipsa corporis tempore consectetur similique fuga, adipisci a pariatur temporibus voluptates inventore dolores cupiditate aliquid officia quibusdam maxime quia? Accusantium?</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum quos, ipsa corporis tempore consectetur similique fuga, adipisci a pariatur temporibus voluptates inventore dolores cupiditate aliquid officia quibusdam maxime quia? Accusantium?</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum quos, ipsa corporis tempore consectetur similique fuga, adipisci a pariatur temporibus voluptates inventore dolores cupiditate aliquid officia quibusdam maxime quia? Accusantium?</p>
+<section>
+        <div class="container-fluid">
+            <div class="row">
+                <img src="https://images.unsplash.com/photo-1542601906990-b4d3fb778b09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1174&q=80" class="w-100" style="height: 550px; object-fit: cover; filter: grayscale(70%);" alt="">
+            </div>
+            <div class="row py-5">
+                <div class="col-md-9 text-center mx-auto">
+                    <h1 class="mb-4">About Us</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias incidunt fugiat illum ipsum culpa modi ab. Illum hic veniam, autem, sapiente temporibus mollitia ratione eaque sed, accusamus enim quos quod!</p>
+                </div>
+            </div>
+            <div class="row py-5" style="background-color: #f5f5f5;">
+                <div class="col-md-10 text-center mx-auto">
+                    <h3>Our Works</h3>
+                    <br>
+                    <div class="row">
+                        <div class="col-md-3">
+                            <span style="font-size: 50px; font-weight:100;letter-spacing: 5px;">1500</span>
+                            <p class="text-uppercase" style="font-size: small;">Projects</p>
+                        </div>
+                        <div class="col-md-3">
+                            <span style="font-size: 50px; font-weight:100;letter-spacing: 5px;">100</span>
+                            <p class="text-uppercase" style="font-size: small;">staffs</p>
+                        </div>
+                        <div class="col-md-3">
+                            <span style="font-size: 50px; font-weight:100;letter-spacing: 5px;">15</span>
+                            <p class="text-uppercase" style="font-size: small;">offices</p>
+                        </div>
+                        <div class="col-md-3">
+                            <span style="font-size: 50px; font-weight:100;letter-spacing: 5px;">3000</span>
+                            <p class="text-uppercase" style="font-size: small;">members</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 p-0">
+                    <img src="https://images.unsplash.com/photo-1482059470115-0aadd6bf6834?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" class="w-100" style="height:610px; object-fit: cover;"alt="">
+                </div>
+                <div class="col-md-6 py-5 text-center mx-auto">
+                    <h3 class="mb-4">Our Missions</h3>
+                    <p>Our vision is an atmosphere that enables people and the planet to thrive – a stabilized climate with warming limited to 1.5ºC and drastically reduced air pollution.</p>
+                    <div class="row mt-5">
+                        <div class="col-md-4 px-0">
+                            <p class="text-center p-3 ">Working at the nexus of climate and air quality, delivering multiple benefits from the mitigation of short-lived climate pollutants: methane, HFCs, black carbon and tropospheric ozone. </p>
+                        </div>
+                        <div class="col-md-4 px-0">
+                            <p class="text-center p-3 ">Strengthening our partnership to deliver results, working arm-in-arm with leaders of national governments, subnational governments, cities, intergovernmental organizations, non-governmental organizations, companies, and civil society.</p>
+                        </div>
+                        <div class="col-md-4 px-0">
+                            <p class="text-center p-3 ">Providing decision-makers the confidence to deliver fast and immediate mitigation.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row" style="background-color: #f5f5f5;">
+                <div class="col-md-6 py-5">
+                    <h3 class="mb-4 text-center">Our Team</h3>
+                    <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. A itaque deleniti voluptate enim eos nostrum, illum inventore rem soluta, expedita quas. Sed, voluptatibus exercitationem. Eveniet dicta aliquid tempora nostrum quod.</p>
+                    <br><br>
+                    <h3 class="mb-4 text-center">Our Story</h3>
+                    <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. A itaque deleniti voluptate enim eos nostrum, illum inventore rem soluta, expedita quas. Sed, voluptatibus exercitationem. Eveniet dicta aliquid tempora nostrum quod.</p>
+                </div>
+                <div class="col-md-6 p-0">
+                    <img src="https://images.unsplash.com/photo-1527525443983-6e60c75fff46?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=632&q=80" style="height: 435px; object-fit: cover;" class="w-100" alt="">
+                </div>
+            </div>
+            <div class="row py-5">
+                <div class="col-md-9 text-center mx-auto">
+                    <h3 class="mb-4">Our Missions</h3>
+                    <p>Our vision is an atmosphere that enables people and the planet to thrive – a stabilized climate with warming limited to 1.5ºC and drastically reduced air pollution.</p>
+                    <div class="row mt-5">
+                        <div class="col-md-4 px-0">
+                            <p class="text-center p-3 ">Working at the nexus of climate and air quality, delivering multiple benefits from the mitigation of short-lived climate pollutants: methane, HFCs, black carbon and tropospheric ozone. </p>
+                        </div>
+                        <div class="col-md-4 px-0">
+                            <p class="text-center p-3 ">Strengthening our partnership to deliver results, working arm-in-arm with leaders of national governments, subnational governments, cities, intergovernmental organizations, non-governmental organizations, companies, and civil society.</p>
+                        </div>
+                        <div class="col-md-4 px-0">
+                            <p class="text-center p-3 ">Providing decision-makers the confidence to deliver fast and immediate mitigation.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
+    </section>
 <?php
-    include("footer.php");
+    include("layout/footer.php");
 ?>
 
